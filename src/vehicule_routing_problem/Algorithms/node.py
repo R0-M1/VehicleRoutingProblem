a839acae22907aa5f764f32a7e1c56b41f5b21db
quentintintind <rexgaming.delaveaud@gmail.com>
2026-03-02 16:18:49 +0100

    premiere implementation

---
+++ src/vehicule_routing_problem/Algorithms/node.py
@@ -0,0 +1,8 @@
+class node:
+    def __init__(self,coordone,readyTime,dueTime,demande,service,estVisite = False):
+        self.coordonne = coordone
+        self.readyTime = readyTime
+        self.dueTime = dueTime
+        self.service = service
+        self.estVisite = self.estVisite
+    
