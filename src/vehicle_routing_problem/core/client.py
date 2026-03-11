7b00d6284e67fb0a560172e8d657dfee037670fc
Romain <romain.aillaud@free.fr>
2026-03-11 09:00:50 +0100

    client

---
+++ src/vehicle_routing_problem/core/client.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+import math
+
+@dataclass(frozen=True)
+class Client:
+    id: int
+    x: float
+    y: float
+    demand: int
+    ready_time: float
+    due_time: float
+    service_time: float
+
+    def distance_to(self, other: "Client") -> float:
+        return math.sqrt((self.x - other.x)**2 + (self.y - other.y)**2)
+
+    @property
+    def is_depot(self) -> bool:
+        return self.id == 0
