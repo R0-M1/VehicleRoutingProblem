787b213f64f6e70d5f21e6a78c26152e3a849dfe
housni2003 <rakotoari.housni@gmail.com>
2026-03-12 14:35:19 +0100

    le main qui marche

--- src/vehicle_routing_problem/main.py
+++ src/vehicle_routing_problem/main.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 from utils.parser import VRPParser
 from vehicle_routing_problem.generator import RandomGenerator, GreedyGenerator
-from visualization.plot_routes import plot_vrptw_solution
 
 if __name__ == "__main__":
     print("--- Lancement du Projet VRPTW ---")
@@ -14,10 +13,8 @@ if __name__ == "__main__":
     generator = RandomGenerator(instance)
     solution = generator.generate()
 
-    fig = plot_vrptw_solution(solution, instance, title="Ma solution optimale")
-    fig.show()  # Interactif dans Jupyter/VSCode
-    fig.write_image("ma_solution.png")  # PNG haute qualité
-
+    print("\n--- Solution Générée ---")
+    print(f"Distance totale : {solution.total_distance:.2f} km")
     # print("\n--- Étape 1 : Sans Time Windows ---")
     # sol_init = generate_random_solution(nodes, capacity, dist_matrix, use_tw=False)
     # print(f"Solution initiale : {sol_init.total_distance(dist_matrix):.2f} km avec {sol_init.total_vehicles()} véhicules.")
